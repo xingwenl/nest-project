c6500a53187ee8492850da3d567ec0e14f089087
xingwen <121380608@qq.com>
2019-10-21 18:25:31 +0800

    mm

--- src/module/api/socket/events.gateway.ts
+++ src/module/api/socket/events.gateway.ts
@@ -3,21 +3,42 @@ import { Server } from 'ws';
 
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+// import { Client } from 'socket.io';
+import {  } from "@nestjs/platform-ws";
 
-@WebSocketGateway(3002, {namespace: 'ws'})
+import {  } from "@nestjs/platform-socket.io";
+import { Client, Socket } from 'socket.io';
+
+@WebSocketGateway(3002, {
+    namespace: 'ws',
+    transports: ['websocket'] 
+})
 export class EventsGateway {
     @WebSocketServer()
     server: Server;
+
+    afterInit(server: Server) {
+        server.emit('events', 'data');
+        console.log('afterInit')
+        
+    }
     
     @SubscribeMessage('events')
-    onEvent(client: any, data: string): Observable<WsResponse<any>> | any {
-        console.log(data)
+    onEvent(client: Socket, data: string): Observable<WsResponse<any>> | any {
+        console.log(client)
+        console.log(client.handshake)
+        console.log(client.id)
         // 通知对应客户端 events 事件
-        client.emit('events', data);
+        // client.server.emit('events', data)
+        client.send('events');
+        Array.from(this.server.clients).forEach(a => {
+            a.send('events12' + data)
+        })
         // 通知其他客户端 chat 事件
         // client.broadcast.emit('events', data);
         // console.log(client)
         // this.server.sockets.emit('events', '我是手表')
+        // this.server.emit('events', { hello: 'world' })
         // return { event: 'events', data: data }
     }
 }
